c9ea1dc8ba0dc75c38a134ee16de0c7537646237
sam kelly <samkelly1290@gmail.com>
2022-05-05 21:58:01 +1000

    wat

--- spx_exchange.c
+++ spx_exchange.c
@@ -423,9 +423,9 @@ struct trader** disconnect(struct trader** traders, struct order** orders, char*
 	}
 
 	if (valid) {
-		printf("%d", cursor);
-		fflush(stdout);
-		traders = realloc(traders, (cursor + 1) * sizeof(struct trader));
+		// printf("%d", cursor);
+		// fflush(stdout);
+		traders = realloc(traders, cursor * sizeof(struct trader));
 		traders[cursor] = NULL;
 		// check the validitiy of this pls
 	}
