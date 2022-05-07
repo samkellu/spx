94051b826724e8a86b09bd0b6ebc3aa3927e6589
sam kelly <samkelly1290@gmail.com>
2022-05-07 21:39:38 +1000

    wow

--- spx_exchange.c
+++ spx_exchange.c
@@ -57,7 +57,7 @@ struct order** delete_order(struct order* del_order, struct order** orders) {
 struct order** cancel_order(struct order* new_order, struct order** orders, int pos_index) {
 
 	int index = 0;
-	while (orders[index]->trader != new_order->trader || orders[index]->order_id != new_order->order_id) {
+	while (orders[index]->trader != new_order->trader && orders[index]->order_id != new_order->order_id) {
 		index++;
 	}
 	orders = delete_order(orders[index], orders);
@@ -80,10 +80,11 @@ struct order** create_order(int type, int pos_index, struct trader* trader, int 
 	if (type == AMEND || type == CANCEL) {
 
 		int cursor = 0;
-		while (orders[cursor]->trader != new_order->trader || orders[cursor]->order_id != new_order->order_id) {
+		while (orders[cursor]->trader != new_order->trader && orders[cursor]->order_id != new_order->order_id) {
 			cursor++;
 		}
 		new_order->type = orders[cursor]->type;
+		printf("%s", orders[cursor]->product);
 		memcpy(new_order->product, orders[cursor]->product, PRODUCT_LENGTH);
 	}
 
