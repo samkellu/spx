0b70ba790bb5ff5e0913eeb12b4f225e92f85be9
sam kelly <samkelly1290@gmail.com>
2022-05-08 17:35:01 +1000

    wow

--- spx_exchange.c
+++ spx_exchange.c
@@ -81,11 +81,11 @@ struct order** create_order(int type, int pos_index, struct trader* trader, int 
 
 		int cursor = 0;
 		while (orders[cursor] != NULL) {
-			printf("%d:%d  %d:%d\n", orders[cursor]->trader->id, new_order->trader->id, orders[cursor]->order_id, new_order->order_id);
+			printf("%d  %d:%d\n", orders[cursor]->trader->id==new_order->trader->id, orders[cursor]->order_id==new_order->order_id);
 			if (orders[cursor]->trader->id == new_order->trader->id && orders[cursor]->order_id == new_order->order_id) {
-				new_order->type = orders[cursor]->type;
 				printf("%s", orders[cursor]->product);
 				printf("wow");
+				new_order->type = orders[cursor]->type;
 				memcpy(new_order->product, orders[cursor]->product, PRODUCT_LENGTH);
 				break;
 			}
