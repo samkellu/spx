2d2d322b50542a87b2b9e39fd937a59327e24e94
sam kelly <samkelly1290@gmail.com>
2022-05-11 19:03:54 +1000

    wow

--- spx_trader.c
+++ spx_trader.c
@@ -40,12 +40,14 @@ int main(int argc, char ** argv) {
         kill(ppid, SIGUSR1);
         struct timespec tim, tim2;
         tim.tv_sec = 0;
-        double nsec = 1000000000 * (pow(2, exponent++)/16);
+        double nsec = 100000000 * (pow(2, exponent++)/16);
         tim.tv_nsec = nsec;
         nanosleep(&tim , &tim2);
 
         if (exponent == 5) {
-          exponent = 4;
+          valid = 1;
+          exponent = 0;
+          continue;
         }
       }
 
@@ -94,8 +96,11 @@ int main(int argc, char ** argv) {
           }
           // +++ should be checking whether the order is accepted and resending if revoked.
           if (valid) {
-            char* msg = malloc(MAX_INPUT);
+
             valid = 0;
+            exponent = 0;
+            
+            char* msg = malloc(MAX_INPUT);
             snprintf(msg, MAX_INPUT, "BUY %d %s %s %s", order_id, args[2], args[3], args[4]);
             write(trader_fd, msg, strlen(msg));
             kill(ppid, SIGUSR1);
