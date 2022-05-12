e8a995c1ef307d932f6ae7855f9350ea29e4814d
sam kelly <samkelly1290@gmail.com>
2022-05-12 17:32:12 +1000

    what

--- spx_exchange.c
+++ spx_exchange.c
@@ -516,11 +516,12 @@ struct trader* initialise_trader(char* path, int index, int num_products) {
 
 	// If the process is the parent, returns the new trader struct
 	if (new_trader->pid > 0) {
-
+		// Waits to catchj failure signal
 		struct timespec tim, tim2;
 		tim.tv_sec = 0;
-		tim.tv_nsec = 100000;
+		tim.tv_nsec = 10000;
 		nanosleep(&tim , &tim2);
+
 		return new_trader;
 
 	} else if (new_trader->pid == 0){
